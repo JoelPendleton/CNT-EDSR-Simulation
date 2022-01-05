Fixed rounding in title of animation. Fixed units

main.py
@@ -2,8 +2,6 @@ import kwant.continuum
 import numpy as np
 import matplotlib.pyplot as plt
 import kwant
-import tkwant
-import scipy.sparse.linalg as sla
 from numpy import vectorize
 from matplotlib import animation
 
@@ -22,15 +20,16 @@ class System:
         self.g = 2
         self.m = 1
         self.e = 1
-        self.total_length_au = self.total_length_m / self.a_0_SI
+        self.total_length_au = self.total_length_m / self.a_0_SI # Total distance of nanotube in terms of au
         self.lattices = lattices
-        self.lattice_size = self.total_length_au / self.lattices
+        self.lattice_size = self.total_length_au / self.lattices # The distance in atomic units spanned by a lattice point
         self.mu_B = self.e * self.hbar / (2 * self.m)
         self.a_0 = 1
-        self.pulse_velocity = 0.1
+        self.pulse_frequency = 240 # in Hz
+        self.pulse_velocity = self.pulse_frequency * self.total_length_au # in au/s
         self.hamiltonian = hamiltonian
         self.template = kwant.continuum.discretize(hamiltonian)
-        self.gaussian_mu = self.lattices/4
+        self.gaussian_mu = 0
         self.a = 1
 
     def tesla_SI_to_au(self, tesla):
@@ -41,11 +40,11 @@ class System:
         """
         return tesla / (self.hbar_SI/(self.e_SI * (self.a_0_SI ** 2)))
 
+    def hartreeToeV(self,hartree):
+        return hartree * 27.2114
 
-
-
-
-
+    def auTonm(self, au):
+        return 0.0529177249 * au
 
     def make_system(self):
         """
@@ -64,33 +63,10 @@ class System:
             (x, ) = site.pos
             return (0 <= x < self.lattices)
 
-
-        def lead_shape(site):
-            """
-            function to define the shape of the leads.
-            :param site: the current site.
-            :return: the a boolean saying whether the lead site should be drawn
-            """
-            (x, ) = site.pos
-            return (0 <= x < self.lattices)
-
         self.syst = kwant.Builder()
 
         #Add the nanotube to the system
         self.syst.fill(self.template, shape, (0, ));
-
-        # #Attach the left gate to the system - for now we assume it's parallel to the nanotube
-        # sym_left_lead = kwant.TranslationalSymmetry((-self.a, ))
-        # left_lead = kwant.Builder(sym_left_lead)
-        # left_lead.fill(self.template, lead_shape, (-self.lattices,))
-        # self.syst.attach_lead(left_lead)
-        #
-        #
-        # #Attach the right gate to the system - for now we assume it's parallel to the nanotube
-        # sym_right_lead = kwant.TranslationalSymmetry((self.a, ))
-        # right_lead = kwant.Builder(sym_right_lead)
-        # right_lead.fill(self.template, lead_shape, (self.lattices+self.a,)) # The leads have the same hamiltonian as the scattering regions
-        # self.syst.attach_lead(right_lead)
 
         kwant.plot(self.syst, file='./figures/shape.png');
         self.syst = self.syst.finalized()
@@ -107,8 +83,7 @@ class System:
         :return: the gaussian function values.
         """
 
-        return np.exp(-np.power(x - self.gaussian_mu - self.pulse_velocity * self.time, 2.) / (2 * np.power(sig, 2.)))
-
+        return np.exp(-np.power(x - self.gaussian_mu - self.pulse_velocity * self.time , 2.) / (2 * np.power(sig, 2.)))
 
     def potential(self, x):  # Infinite square well
         """
@@ -116,11 +91,19 @@ class System:
         :param x: the position in the system.
         :return: the potential energy.
         """
-        if 0 <= x <= self.lattices:
-            return 0.002 * self.gaussian(x, self.lattices / 14)
+        if 0 <= x <= self.total_length_au:
+            return 0.002 * self.gaussian(x, self.total_length_au / 14)
         else:
             return 999999999
 
+    def kwantPotential(self, x):
+        """
+        Potential that kwant uses. It inputs lattice points coord. so we need to convert to au to use our
+        other functions
+        :param x:
+        :return: potential after conversion to au
+        """
+        return self.potential(x * self.lattice_size) # Kwant uses lattice points so need scaling
 
     def eigenstates(self):
         """
@@ -133,7 +116,7 @@ class System:
         self.A_constant =  -self.g * self.mu_B * B_0_au * self.hbar / 2
         self.B_constant = -self.g * self.mu_B * b_sl_au * self.hbar / 2
         self.C_constant = self.hbar ** 2 / (2 * self.m)
-        params = dict(A=self.A_constant, B=self.B_constant, C=self.C_constant, V=self.potential)
+        params = dict(A=self.A_constant, B=self.B_constant, C=self.C_constant, V=self.kwantPotential)
         # Calculate the wave functions in the system.
         h = self.syst.hamiltonian_submatrix(params=params)
 
@@ -143,11 +126,9 @@ class System:
         eigenValues = np.real(eigenValues[idx])
         eigenVectors = eigenVectors[:, idx]
 
-        # print(eigenValues) # Eigenvalues are repeated
         # For each eigenvalue for each spin we get two eigenvectors and for each position there are two possible spins
         # so we have four eigenvectors for each position along the nanotube.
-        # print(np.abs(eigenVectors[0::2,1])**2 + np.abs(eigenVectors[1::2,1])**2 +
-        #       np.abs(eigenVectors[0::2,2])**2+ np.abs(eigenVectors[1::2,2])**2)
+
 
         return eigenValues, eigenVectors
 
@@ -157,8 +138,9 @@ class System:
         :param syst: the system object.
         :return:
         """
+        self.gaussian_mu = self.total_length_au /4
         eigenValues, eigenVectors = self.eigenstates()
-        x_coordinates = np.linspace(0, self.lattices, self.lattices)
+        x_coordinates = np.linspace(0, self.total_length_au , self.lattices)
         vpotential = vectorize(self.potential)
 
         y_coordinates = vpotential(x_coordinates)
@@ -169,9 +151,9 @@ class System:
         E_3 = eigenValues[2]
 
         # print("The energies of the ground and excited states are {0} and {1}, respectively.".format(E_1, E_2))
-        plt.plot([0,100], [E_1, E_1], label="$E_1$")
-        plt.plot([0,100], [E_2, E_2], label="$E_2$")
-        plt.plot([0,100], [E_3, E_3], label="$E_3$", linestyle='dashed')
+        plt.plot([0, np.max(x_coordinates)], [E_1, E_1], label="$E_1$")
+        plt.plot([0, np.max(x_coordinates)], [E_2, E_2], label="$E_2$")
+        plt.plot([0, np.max(x_coordinates)], [E_3, E_3], label="$E_3$", linestyle='dashed')
         plt.xlabel("$x (au$)")
         plt.ylabel("$E (H)$")
         plt.legend(loc="upper right")
@@ -191,22 +173,20 @@ class System:
 
 
         x_coords = np.linspace(0, self.total_length_au, self.lattices)
+
         psi1 = np.abs(eigenVectors[0::2,0])**2 + np.abs(eigenVectors[1::2,0])**2 +\
-               np.abs(eigenVectors[0::2,1])**2+ np.abs(eigenVectors[1::2, 1])**2
+                   np.abs(eigenVectors[0::2,1])**2+ np.abs(eigenVectors[1::2, 1])**2
+
+        psi2 = np.abs(eigenVectors[0::2, 2]) ** 2 + np.abs(eigenVectors[1::2, 2]) ** 2 + \
+               np.abs(eigenVectors[0::2, 3]) ** 2 + np.abs(eigenVectors[1::2, 3]) ** 2
 
         if animate:
-            return x_coords,psi1
+            return x_coords,psi1, psi2
 
         else:
             plt.figure()
-
             plt.plot(x_coords, psi1, label="n=1")
-
-            psi2 = np.abs(eigenVectors[0::2,2])**2 + np.abs(eigenVectors[1::2,2])**2 +\
-                   np.abs(eigenVectors[0::2,3])**2+ np.abs(eigenVectors[1::2,3])**2
-
             plt.plot(x_coords, psi2, label="n=2")
-
 
 
             plt.xlabel("x (au)")
@@ -220,37 +200,61 @@ class System:
 
 
     def animateWavefunction(self):
+        self.gaussian_mu = 0 # self.total_length_au / 4
+        # create a figure with two subplots
+        fig, (ax1, ax2) = plt.subplots(2, 1)
+        x, y1,y2 = self.showWavefunction(animate=True)
+        vpotential = vectorize(self.potential)
+        y3 = vpotential(x)
 
-        fig = plt.figure()
-        x, y = self.showWavefunction(animate=True)
+        # intialize two line objects (one in each axes)
+        line1, = ax1.plot([], [], lw=2, label='$n=1$')
+        line2, = ax1.plot([], [], lw=2,label='$n=2$')
+        line3, = ax2.plot([], [], lw=2, color='r', label='$V(x)$')
+        line = [line1, line2, line3]
+        xmax = self.auTonm(np.max(x))
+        ax1.set_xlim(0, xmax)
+        ax1.set_ylim(0, np.max(y1) * 1.2)
+        ax2.set_xlim(0, xmax)
+        ax2.set_ylim(0, self.hartreeToeV(np.max(y3) * 1.5))
 
-        ax = plt.axes(xlim=(np.min(x), np.max(x)), ylim=(np.min(y), np.max(y) * 1.2))
-
-        line, = ax.plot([], [], lw=2)
-        plt.xlabel("x (au)")
-        plt.ylabel("$|\psi(x)|^2$")
-        plt.title("Animation of $n = 1$ Wave Function")
-
-        self.gaussian_mu = 0
+        plt.xlabel("x (nm)")
+        ax1.set_ylabel("$|\psi(x)|^2$")
+        ax2.set_ylabel("$E$ (eV)")
+        fig.suptitle("Wave Functions Inside a Time-varying Position at $t=0s$")
+        number_of_frames = 200  # Takes [number_of_frames] time steps at frequency provided for pulse to travel along the
+                               # the nanotube.
 
         # initialization function: plot the background of each frame
         def init():
-            line.set_data([], [])
-            return line,
+            line[0].set_data([], [])
+            line[1].set_data([], [])
+            line[2].set_data([], [])
+
+            return line
 
         def animate(i):
-            self.time = i
-            line.set_label('$t = {}$'.format(i))
-            plt.legend(loc="upper right")
+            self.time =  (i / (self.pulse_frequency)) / number_of_frames
+            fig.suptitle("Wave Functions Inside a Time-varying Position at $t={}s$".format('{:g}'.format(float('{:.{p}g}'.format(self.time, p=2)))))
 
-            x, y = self.showWavefunction(animate=True)
-            line.set_data(x, y)
 
-            return line,
+
+            x, y1,y2 = self.showWavefunction(animate=True)
+
+            vpotential = vectorize(self.potential)
+            y3 = vpotential(x)
+            x = self.auTonm(x)
+            line[0].set_data(x, y1)
+            line[1].set_data(x, y2)
+            line[2].set_data(x, self.hartreeToeV(y3))
+            ax1.legend(loc="upper right")
+            ax2.legend(loc="upper right")
+
+            return line
 
         # call the animator.  blit=True means only re-draw the parts that have changed.
-        anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                       frames=1000, interval=10, blit=True)
+        anim = animation.FuncAnimation(fig, animate,init_func=init,
+                                       frames=number_of_frames, interval=30, blit=True)
 
         # save the animation as an mp4.  This requires ffmpeg or mencoder to be
         # installed.  The extra_args ensure that the x264 codec is used, so that
@@ -268,8 +272,8 @@ def main():
     system1.make_system()
     system1.animateWavefunction()
     system1.time = 0
+    system1.gaussian_mu = system1.lattices * system1.lattice_size / 4
     system1.showEnergies()
-
     system1.showWavefunction()
 
 