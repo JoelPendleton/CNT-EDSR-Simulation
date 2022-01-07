d0626f60deae09353dd3699d293c6d713e4b4fff
Joel Pendleton <joelkieronpendleton@gmail.com>
2022-01-07 15:47:42 +0000

    Understood how to use the eigenvectors to give the wave function and pdf. The code has been modified to reflect this.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import kwant
 from numpy import vectorize
 from matplotlib import animation
+from scipy import integrate
+from tkwant import onebody
 
 class System:
     def __init__(self, hamiltonian, lattices):
@@ -12,8 +14,8 @@ class System:
         self.e_SI = 1.602176634e-19
         self.a_0_SI = 5.2917721090380e-11
         self.total_length_m = 7820e-9
-        self.B_0_SI = 100e-3
-        self.b_sl_SI = 250e-3
+        self.B_0_SI = 10e-3
+        self.b_sl_SI = 2500e-3
         self.time = 0
         # Constants in a.u.
         self.hbar = 1
@@ -31,6 +33,7 @@ class System:
         self.template = kwant.continuum.discretize(hamiltonian)
         self.gaussian_mu = 0
         self.a = 1
+        self.pulse_type = 1
 
     def tesla_SI_to_au(self, tesla):
         """
@@ -63,13 +66,38 @@ class System:
             (x, ) = site.pos
             return (0 <= x < self.lattices)
 
+        # def lead_shape(site):
+        #     """
+        #     function to define the shape of the leads.
+        #     :param site: the current site.
+        #     :return: the a boolean saying whether the lead site should be drawn
+        #     """
+        #     (x,) = site.pos
+        #     return (0 <= x < self.lattices)
+
+
         self.syst = kwant.Builder()
 
         #Add the nanotube to the system
         self.syst.fill(self.template, shape, (0, ));
 
+        #
+        # #Attach the left gate to the system - for now we assume it's parallel to the nanotube
+        # sym_left_lead = kwant.TranslationalSymmetry((-self.a, ))
+        # left_lead = kwant.Builder(sym_left_lead)
+        # left_lead.fill(self.template, lead_shape, (-self.lattices,))
+        # self.syst.attach_lead(left_lead)
+        #
+        #
+        # #Attach the right gate to the system - for now we assume it's parallel to the nanotube
+        # sym_right_lead = kwant.TranslationalSymmetry((self.a, ))
+        # right_lead = kwant.Builder(sym_right_lead)
+        # right_lead.fill(self.template, lead_shape, (self.lattices+self.a,)) # The leads have the same hamiltonian as the scattering regions
+        # self.syst.attach_lead(right_lead)
+
         kwant.plot(self.syst, file='./figures/shape.png');
         self.syst = self.syst.finalized()
+
         return self.syst
 
 
@@ -91,8 +119,17 @@ class System:
         :param x: the position in the system.
         :return: the potential energy.
         """
+
         if 0 <= x <= self.total_length_au:
-            return 0.002 * self.gaussian(x, self.total_length_au / 14)
+            if self.pulse_type == 1:
+                self.pulse_velocity = self.pulse_frequency * self.total_length_au  # in au/s
+                return 0.002 * self.gaussian(x, self.total_length_au / 14)
+            elif self.pulse_type == 2:
+                self.pulse_velocity = 0
+                self.gaussian_mu = self.total_length_au / 4
+                return 0.002 * (np.cos(1000 * np.pi * self.time) **2) * self.gaussian(x, self.total_length_au / 14)
+            else:
+                return 0
         else:
             return 999999999
 
@@ -111,20 +148,20 @@ class System:
         :param syst: the system object.
         :return: the sorted eigenvalues and eigenvectors.
         """
-        B_0_au = self.tesla_SI_to_au(self.B_0_SI)
-        b_sl_au = self.tesla_SI_to_au(self.b_sl_SI)  # in hbar/(e*(a_0)**2)
-        self.A_constant =  -self.g * self.mu_B * B_0_au * self.hbar / 2
-        self.B_constant = -self.g * self.mu_B * b_sl_au * self.hbar / 2
+        self.B_0_au = self.tesla_SI_to_au(self.B_0_SI)
+        self.b_sl_au = self.tesla_SI_to_au(self.b_sl_SI)  # in hbar/(e*(a_0)**2)
+        self.A_constant =  -self.g * self.mu_B * self.B_0_au * self.hbar / 2
+        self.B_constant = -self.g * self.mu_B * self.b_sl_au * self.hbar / 2
         self.C_constant = self.hbar ** 2 / (2 * self.m)
         params = dict(A=self.A_constant, B=self.B_constant, C=self.C_constant, V=self.kwantPotential)
         # Calculate the wave functions in the system.
-        h = self.syst.hamiltonian_submatrix(params=params)
-
-        eigenValues, eigenVectors = np.linalg.eig(h)
+        hamiltonian = self.syst.hamiltonian_submatrix(params=params)
+        eigenValues, eigenVectors = np.linalg.eig(hamiltonian)
 
         idx = eigenValues.argsort()
-        eigenValues = np.real(eigenValues[idx])
+        eigenValues = eigenValues[idx]
         eigenVectors = eigenVectors[:, idx]
+
 
         # For each eigenvalue for each spin we get two eigenvectors and for each position there are two possible spins
         # so we have four eigenvectors for each position along the nanotube.
@@ -138,28 +175,30 @@ class System:
         :param syst: the system object.
         :return:
         """
-        self.gaussian_mu = self.total_length_au /4
         eigenValues, eigenVectors = self.eigenstates()
         x_coordinates = np.linspace(0, self.total_length_au , self.lattices)
-        vpotential = vectorize(self.potential)
 
-        y_coordinates = vpotential(x_coordinates)
-        plt.figure()
-        plt.plot(x_coordinates, y_coordinates, label="$V(x)$")
-        E_1 = eigenValues[0]
-        E_2 = eigenValues[1]
-        E_3 = eigenValues[2]
+        fig = plt.figure()
+        E_1 = np.real(eigenValues[0])
+        E_2 = np.real(eigenValues[1])
+        E_3 = np.real(eigenValues[2])
+        E_4 = np.real(eigenValues[3])
+        E_5 = np.real(eigenValues[4])
+
 
         # print("The energies of the ground and excited states are {0} and {1}, respectively.".format(E_1, E_2))
         plt.plot([0, np.max(x_coordinates)], [E_1, E_1], label="$E_1$")
         plt.plot([0, np.max(x_coordinates)], [E_2, E_2], label="$E_2$")
         plt.plot([0, np.max(x_coordinates)], [E_3, E_3], label="$E_3$", linestyle='dashed')
+        plt.plot([0, np.max(x_coordinates)], [E_4, E_4], label="$E_4$", linestyle='dashed')
+        plt.plot([0, np.max(x_coordinates)], [E_5, E_5], label="$E_5$", linestyle='dashed')
+
         plt.xlabel("$x (au$)")
         plt.ylabel("$E (H)$")
         plt.legend(loc="upper right")
-        plt.savefig("./figures/Energies/energies-{}.svg".format(self.time))  # With A = 0 we expect straight forward zeeman splitting
-        plt.close()
-        print("Plot of energies saved.")
+        plt.savefig("./figures/Energies/energies.svg")  # With A = 0 we expect straight forward zeeman splitting
+        plt.close(fig)
+        print("Plot of eigenenergies saved.")
 
     def showWavefunction(self, animate=False):
 
@@ -171,30 +210,50 @@ class System:
 
         eigenValues, eigenVectors = self.eigenstates()
 
-
         x_coords = np.linspace(0, self.total_length_au, self.lattices)
 
-        psi1 = np.abs(eigenVectors[0::2,0])**2 + np.abs(eigenVectors[1::2,0])**2 +\
-                   np.abs(eigenVectors[0::2,1])**2+ np.abs(eigenVectors[1::2, 1])**2
+        psi1 = eigenVectors[:, 0]
+        psi1_up, psi1_down = psi1[::2], psi1[1::2]
+        # even indices give the spin up and odd indices give the spin down states
+        density_1 = np.abs(psi1_up) ** 2 + np.abs(psi1_down) ** 2
+        psi2 = eigenVectors[:, 1]
+        psi2_up, psi2_down = psi2[::2], psi2[1::2]
+        density_2 = np.abs(psi2_up) ** 2 + np.abs(psi2_down) ** 2
 
-        psi2 = np.abs(eigenVectors[0::2, 2]) ** 2 + np.abs(eigenVectors[1::2, 2]) ** 2 + \
-               np.abs(eigenVectors[0::2, 3]) ** 2 + np.abs(eigenVectors[1::2, 3]) ** 2
+        # https://kwant-project.org/doc/dev/tutorial/operators - this explains the output of the eigenvectors.
+        self.sigma_y = np.array([[0 , -1j],
+                                 [1j, 0]])
+
+
+        rho_sy = kwant.operator.Density(self.syst, self.sigma_y)
+
+        # calculate the expectation values of the operators with 'psi'
+
+        spin_y = rho_sy(psi1)
+        # Need to integrate, then get average mag field experienced by the the electron
+        # Need to store this for different times
+        y_int = np.trapz(spin_y, x=x_coords)
+        # Then once we have that can compute the transition probability at different times.
+        print(y_int)
 
         if animate:
-            return x_coords,psi1, psi2
+            return x_coords,density_1, density_2
 
         else:
-            plt.figure()
-            plt.plot(x_coords, psi1, label="n=1")
-            plt.plot(x_coords, psi2, label="n=2")
+            fig = plt.figure()
+            plt.plot(self.auTonm((x_coords)), density_1, label="$\psi_{G_{- }}$")
+            plt.plot(self.auTonm((x_coords)), density_2, label="$\psi_{G_{+ }}$")
 
 
-            plt.xlabel("x (au)")
+
+
+
+            plt.xlabel("x (nm)")
             plt.ylabel("$|\psi(x)|^2$")
             plt.legend(loc="upper right")
             plt.savefig("./figures/PDFs/pdf-{}.svg".format(self.time))  # With A = 0 we expect straight forward zeeman splitting
-            plt.close()
-            print("Plot of wave functions saved!")
+            plt.close(fig)
+            print("Plot of wave functions saved at at t={}s".format('{:g}'.format(float('{:.{p}g}'.format(self.time, p=2)))))
             return True
 
 
@@ -222,7 +281,7 @@ class System:
         ax1.set_ylabel("$|\psi(x)|^2$")
         ax2.set_ylabel("$E$ (eV)")
         fig.suptitle("Wave Functions Inside a Time-varying Position at $t=0s$")
-        number_of_frames = 200  # Takes [number_of_frames] time steps at frequency provided for pulse to travel along the
+        number_of_frames = 120  # Takes [number_of_frames] time steps at frequency provided for pulse to travel along the
                                # the nanotube.
 
         # initialization function: plot the background of each frame
@@ -236,8 +295,6 @@ class System:
         def animate(i):
             self.time =  (i / (self.pulse_frequency)) / number_of_frames
             fig.suptitle("Wave Functions Inside a Time-varying Position at $t={}s$".format('{:g}'.format(float('{:.{p}g}'.format(self.time, p=2)))))
-
-
 
             x, y1,y2 = self.showWavefunction(animate=True)
 
@@ -261,18 +318,24 @@ class System:
         # the video can be embedded in html5.  You may need to adjust this for
         # your system: for more information, see
         # http://matplotlib.sourceforge.net/api/animation_api.html
-        anim.save('wavefunction-animation.mp4', writer='ffmpeg')
-        plt.close()
-        print("Animation of wave function saved!")
+        if self.pulse_type == 1:
+            anim.save('wavefunction-animation-pulse-1.mp4', writer='ffmpeg')
+        elif self.pulse_type ==2:
+            anim.save('wavefunction-animation-pulse-2.mp4', writer='ffmpeg')
+
+        plt.close(fig)
+        print("Animation of wave functions saved.")
 
 def main():
     lattices = 100
 
     system1 = System("(C * k_x**2 + V(x)) * identity(2) + A * sigma_x + B * x * sigma_y", lattices)
     system1.make_system()
-    system1.animateWavefunction()
-    system1.time = 0
-    system1.gaussian_mu = system1.lattices * system1.lattice_size / 4
+    # system1.pulse_type = 2
+    # system1.animateWavefunction()
+    # system1.time = 0
+    # system1.pulse_type = 0
+
     system1.showEnergies()
     system1.showWavefunction()
 
